32350e0f2332b4e9f0fd2a605f8c773e7e24e28a
Woodhouse <pariswoodhouse@hotmail.com>
2021-12-15 20:25:38 -0800

    reworked grid generator function

--- main.js
+++ main.js
@@ -1,24 +1,30 @@
 gridGenerator();
 const grid = document.querySelectorAll('.grid');
 
-function gridGenerator() {
+// Function to generate a 16x16 grid
+function gridGenerator(rows = 16, columns = 16) {
   const container = document.querySelector('.container');
-  for (let i = 0; i < 256; i++) {
-    const element = document.createElement('div');
-    element.classList.add('grid');
-    container.appendChild(element);
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
+      const element = document.createElement('div');
+      element.classList.add('grid');
+      container.appendChild(element);
+    }
   }
 }
 
+// Event listener to colour grid black on hover
 grid.forEach((gridItem) => {
   gridItem.addEventListener('mouseover', () => {
-    gridItem.classList.add('active')
+    gridItem.classList.add('active');
   });
 });
 
-const clearButton = document.querySelector('button')
+// Clear button functionality
+const clearButton = document.querySelector('.clear');
 clearButton.addEventListener('click', () => {
-  grid.forEach(gridItem => {
-    gridItem.classList.remove('active')
-  })
-})
+  grid.forEach((gridItem) => {
+    gridItem.classList.remove('active');
+  });
+});
