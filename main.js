ffa2660ea791974f79e57155d3c5306f7e28766c
Woodhouse <pariswoodhouse@hotmail.com>
2021-12-18 15:22:24 -0800

    New grid button created and delete grid function

--- main.js
+++ main.js
@@ -1,30 +1,55 @@
 gridGenerator();
-const grid = document.querySelectorAll('.grid');
+colourBlack();
 
 // Function to generate a 16x16 grid
 function gridGenerator(rows = 16, columns = 16) {
   const container = document.querySelector('.container');
-
+  const grid = document.querySelectorAll('.grid');
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < columns; j++) {
       const element = document.createElement('div');
       element.classList.add('grid');
       container.appendChild(element);
+      container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+      container.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
     }
   }
 }
 
-// Event listener to colour grid black on hover
-grid.forEach((gridItem) => {
-  gridItem.addEventListener('mouseover', () => {
-    gridItem.classList.add('active');
+function colourBlack() {
+  const grid = document.querySelectorAll('.grid');
+  grid.forEach((gridItem) => {
+    gridItem.addEventListener('mouseover', () => {
+      gridItem.classList.add('active');
+    });
   });
-});
+}
 
 // Clear button functionality
 const clearButton = document.querySelector('.clear');
 clearButton.addEventListener('click', () => {
+  const grid = document.querySelectorAll('.grid');
   grid.forEach((gridItem) => {
     gridItem.classList.remove('active');
   });
 });
+
+const newGridButton = document.querySelector('.new-grid');
+newGridButton.addEventListener('click', () => {
+  const newGrid = prompt('Please enter new grid');
+  if (newGrid <= 100) {
+    deleteGrid();
+    gridGenerator(newGrid, newGrid);
+    colourBlack();
+  } else {
+    alert('Please enter a number below 100')
+  }
+  
+});
+
+function deleteGrid() {
+  const grid = document.querySelectorAll('.grid');
+  grid.forEach((gridItem) => {
+    gridItem.remove();
+  });
+}
